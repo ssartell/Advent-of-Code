081d75648b6947f72721fb94bbe18809799f08af
ssartell <ssartell@rightpoint.com>
2015-12-05 14:38:44 -0600

    day 5

--- day5/part1.js
+++ day5/part1.js
@@ -1,18 +1,16 @@
 var R = require('ramda');
+
+var parseInput = R.split('\n');
+var trace = R.tap(console.log);
+
 var letters = R.split('', 'abcdefghijklmnopqrstuvwxyz');
 var doubleLetters = R.map(R.compose(R.apply(R.concat), R.flip(R.repeat)(2)), letters);
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var disallowed = ['ab', 'cd', 'pq', 'xy'];
 
-var parseInput = R.split('\n');
-var trace = R.tap(console.log);
-
 var boolToInt = function(bool) {
 	return +bool;
 };
-var contains = R.curry(function(subStr, str) {
-	return str.indexOf(subStr) > -1;
-});
 var occurrences = R.curry(function(subStr, str) {
 	return R.match(new RegExp('(' + subStr + ')', 'g'), str).length;
 });
@@ -24,6 +22,6 @@ var notContainsDisallowed = R.compose(R.equals(0), numberOf(disallowed));
 var containsAnyDoubleLetters = R.compose(R.gt(R.__, 0), numberOf(doubleLetters));
 
 var isNiceWord = R.allPass([containsThreeVowels, notContainsDisallowed, containsAnyDoubleLetters]);
-var solution = R.compose(R.sum, R.map(boolToInt), R.map(isNiceWord), parseInput);
+var solution = R.compose(R.sum, R.map(R.compose(boolToInt, isNiceWord)), parseInput);
 
 module.exports = solution;
