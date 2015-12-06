d35d9c4b61788f64bf115160f77bc5e19d78b86e
ssartell <ssartell@rightpoint.com>
2015-12-05 22:59:55 -0600

    switch day 5 to pure regex

--- day5/part1.js
+++ day5/part1.js
@@ -2,22 +2,11 @@ var R = require('ramda');
 
 var parseInput = R.split('\n');
 var trace = R.tap(console.log);
-
-var letters = R.split('', 'abcdefghijklmnopqrstuvwxyz');
-var doubleLetters = R.map(R.compose(R.apply(R.concat), R.flip(R.repeat)(2)), letters);
-var vowels = ['a', 'e', 'i', 'o', 'u'];
-var disallowed = ['ab', 'cd', 'pq', 'xy'];
-
 var boolToInt = (bool) => +bool;
-var toRegExp = R.compose(R.curry(RegExp)(R.__, 'g'), R.concat(R.__, ')'), R.concat('('));
-var matches = R.curry(R.binary(R.converge(R.match, [toRegExp, R.nthArg(1)])));
-var occurrences = R.curry(R.compose(R.prop('length'), matches));
 
-var numberOf = R.compose(R.converge(R.unapply(R.sum)), R.map(occurrences));
-
-var containsThreeVowels = R.compose(R.gte(R.__, 3), numberOf(vowels));
-var notContainsDisallowed = R.compose(R.equals(0), numberOf(disallowed));
-var containsAnyDoubleLetters = R.compose(R.gt(R.__, 0), numberOf(doubleLetters));
+var containsThreeVowels = R.test(/[aeiou].*[aeiou].*[aeiou]/g);
+var notContainsDisallowed = R.compose(R.not, R.test(/ab|cd|pq|xy/g));
+var containsAnyDoubleLetters = R.test(/(.)\1+/g);
 
 var isNiceWord = R.allPass([containsThreeVowels, notContainsDisallowed, containsAnyDoubleLetters]);
 var solution = R.compose(R.sum, R.map(R.compose(boolToInt, isNiceWord)), parseInput);
--- day5/part2.js
+++ day5/part2.js
@@ -2,21 +2,10 @@ var R = require('ramda');
 
 var parseInput = R.split('\n');
 var trace = R.tap(console.log);
-
-var letters = R.split('', 'abcdefghijklmnopqrstuvwxyz');
-var letterPairs = R.map(R.apply(R.concat), R.xprod(letters, letters));
-var spacedLetterPairs = R.map(R.converge(R.concat, [R.identity, R.head]), letterPairs);
-
 var boolToInt = (bool) => +bool;
-var toRegExp = R.compose(R.curry(RegExp)(R.__, 'g'), R.concat(R.__, ')'), R.concat('('));
-var matches = R.curry(R.binary(R.converge(R.match, [toRegExp, R.nthArg(1)])));
-var occurrences = R.curry(R.compose(R.prop('length'), matches));
 
-var numberOf = R.compose(R.converge(R.unapply(R.sum)), R.map(occurrences));
-var anyTwice = R.compose(R.converge(R.unapply(R.any(R.gte(R.__, 2)))), R.map(occurrences));
-
-var containsSpacedLetterPair = R.compose(R.gt(R.__, 0), numberOf(spacedLetterPairs));
-var containsAnyPairTwice = anyTwice(letterPairs);
+var containsSpacedLetterPair = R.test(/(.).\1/g);
+var containsAnyPairTwice = R.test(/(..).*\1/g);
 
 var isNiceWord = R.allPass([containsSpacedLetterPair, containsAnyPairTwice]);
 var solution = R.compose(R.sum, R.map(R.compose(boolToInt, isNiceWord)), parseInput);
